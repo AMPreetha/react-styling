71657bf3dde8568614a5a5a06fe61133048bd132
AMPreetha <127812727+AMPreetha@users.noreply.github.com>
2023-04-04 08:00:30 +0530

    Add files via upload

---
+++ coursegoal.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import './course.css';
+import Button from '/components/Button';
+
+
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const goalInputChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
